9f7dad57129957296f46b57bba3bb57291b6709f
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-10-11 18:38:28 +0300

    add link to categories list

--- src/js/categories-api/render.js
+++ src/js/categories-api/render.js
@@ -3,7 +3,13 @@ const categoriesList = document.querySelector('.js-categories');
 
 async function renderCategories(data) {
   const markup = await data
-    .map(({ list_name }) => ` <li class="categories__item">${list_name}</li>`)
+    .map(({ list_name }) => {
+      return `
+      <li class="categories__item">
+        <a href="#" class="categories__link link">
+           ${list_name}
+      </a>
+    </li>`})
     .join('');
 
   categoriesList.insertAdjacentHTML('beforeend', markup);
