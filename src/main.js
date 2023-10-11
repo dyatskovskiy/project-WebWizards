41a8c42e3fd1ec50c679d763df64db5bd70ab5c5
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-10-11 18:00:49 +0300

    finish script

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 // import scripts here
+// Скрипти для рендеру категорій
+import './js/categories-api/categories-api';
+import './js/categories-api/render';
+//_________________________________________
